242d8919fb2537749df06fdea5ebf997d253b7bf
Quang Hung <quanghungleo@gmail.com>
2018-10-28 21:51:16 +0700

    First

---
+++ MyNetCoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyNetCoreMVC.Models;
+using Newtonsoft.Json;
+
+namespace MyNetCoreMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetList()
+        {
+            return View();
+        }
+        public IActionResult Create(string name, int price)
+        {
+            Product product = new Product
+            {
+                name = name,
+                price = price
+            };
+            ViewBag.product = product;
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            //ViewData["ID"] = id;
+            Product product = new Product {
+                id = id,
+            };
+            return Json(product);
+        }
+    }
+}
